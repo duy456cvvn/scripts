0051c873d2b1feda1e3114ec4c424733793e65ce
duy456cvvn <52267006+duy456cvvn@users.noreply.github.com>
2020-04-03 17:01:34 +0700

    Update AModdedDay.lua

--- AModdedDay.lua
+++ AModdedDay.lua
@@ -1,5 +1,5 @@
 --[[
-Script moved to https://discord.gg/XWuUNxB
+Script moved to https://discord.gg/58ATauA
 ]]
 
-game.Players.LocalPlayer:Kick("Script move : https://discord.gg/XWuUNxB")
+game.Players.LocalPlayer:Kick("Script move : https://discord.gg/58ATauA")
